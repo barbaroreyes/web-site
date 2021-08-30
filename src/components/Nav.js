7e8cbb924d5426779f74dd36184fa788a0386ec1
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-08-30 18:28:12 -0500

    added-color

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,8 +2,12 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 const Nav = () => {
   return (
-    <div>
-      <Link to='/portafolio'>Portafolio</Link>
+    <div className="nav">
+     <Link to='/'>Home</Link>
+     <Link to='/portafolio'>Portafolio</Link>
+     <Link to='/about'>About</Link>
+     <Link to='/contact'>Contact</Link>
+     
     </div>
   )
 }
